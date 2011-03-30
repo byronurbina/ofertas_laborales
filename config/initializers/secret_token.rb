9535f8946460adc41eb81955d33c45a5922ec148
byronurbina <byron_urbina5@hotmail.com>
2011-03-30 11:29:30 -0600

    priemro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfertasLaborales::Application.config.secret_token = 'fc915ca48cab2705b4550b251f417b58b149e1dd21e2fef7624c27ec9b6c0e47a91fb70966b97285dfc0ca5e3e8d6182a8edf50bd73d673e1d7922c6fc89a9a7'
